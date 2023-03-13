352fc06a9184037dfb33537f63fdffe7358af8b2
Adam Okoń <adam.okon@elegantcode.pl>
2023-03-13 22:49:15 +0100

    play with consts

--- rust/assembly-line/src/lib.rs
+++ rust/assembly-line/src/lib.rs
@@ -1,7 +1,10 @@
+const BASE_PRODUCTION: f64 = 221.0;
+const SIXTY_MINUTES: u32 = 60;
+
 pub fn production_rate_per_hour(speed: u8) -> f64 {
     let ratio: f64;
-    let num_of_car: f64 = 221.0;
 
+    // pattern matching for ranges is only available in nighty version
     if speed < 5 {
         ratio = 1.0;
     } else if speed >= 5 && speed < 9 {
@@ -10,11 +13,11 @@ pub fn production_rate_per_hour(speed: u8) -> f64 {
         ratio = 0.77;
     }
 
-    speed as f64 * num_of_car * ratio
+    speed as f64 * BASE_PRODUCTION * ratio
 }
 
 pub fn working_items_per_minute(speed: u8) -> u32 {
     let hourly_car_num = production_rate_per_hour(speed) as u32;
 
-    hourly_car_num / 60
+    hourly_car_num / SIXTY_MINUTES
 }
