c64955653d2cbc69d2ddcd5a95a5bbff41a9ce7b
Adam Okoń <adam.okon@elegantcode.pl>
2024-06-20 23:05:37 +0200

    add solution for go/party-robot

--- go/party-robot/party_robot.go
+++ go/party-robot/party_robot.go
@@ -1,6 +1,8 @@
 package partyrobot
 
-import "fmt"
+import (
+	"fmt"
+)
 
 // Welcome greets a person by name.
 func Welcome(name string) string {
@@ -12,8 +14,25 @@ func HappyBirthday(name string, age int) string {
 	return fmt.Sprintf("Happy birthday %s! You are now %v years old!", name, age)
 }
 
+func formatTable(table int) string {
+	var result string
+
+	switch {
+	case table < 10:
+		result = fmt.Sprintf("00%d", table)
+	case table >= 10 && table < 100:
+		result = fmt.Sprintf("0%d", table)
+	default:
+		result = fmt.Sprintf("%d", table)
+	}
+
+	return result
+}
+
 // AssignTable assigns a table to each guest.
 func AssignTable(name string, table int, neighbor, direction string, distance float64) string {
-  parts := []
-	return ""
+	return fmt.Sprintf("Welcome to my party, %s!\n", name) +
+		fmt.Sprintf("You have been assigned to table %s. ", formatTable(table)) +
+		fmt.Sprintf("Your table is %s, exactly %.1f meters from here.\n", direction, distance) +
+		fmt.Sprintf("You will be sitting next to %s.", neighbor)
 }
